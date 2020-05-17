89f0e04f858c208fe8dc0fa19f2b272e9511f9ce
Laddeus <33806862+Laddeus@users.noreply.github.com>
2020-05-18 00:01:24 +0300

    changes with names

---
+++ B20_Ex02_1/BoardLetter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B20_Ex02
+{
+    internal class BoardLetter
+    {
+        private char m_Letter;
+        private bool m_IsHidden;
+
+        public BoardLetter(char i_Letter, bool i_IsHidden)
+        {
+            m_Letter = i_Letter;
+            m_IsHidden = i_IsHidden;
+        }
+
+        public char Letter
+        {
+            get => m_Letter;
+            set => m_Letter = value;
+        }
+        public bool IsHidden
+        {
+            get => m_IsHidden;
+            set => m_IsHidden = value;
+        }
+    }
+}
---
+++ B20_Ex02_1/Cell.cs
@@ -0,0 +1,25 @@
+﻿namespace B20_Ex02
+{
+    public struct Cell
+    {
+        private int m_Row;
+        private int m_Column;
+
+        public Cell(int i_Row, int i_Column)
+        {
+            m_Row = i_Row;
+            m_Column = i_Column;
+        }
+
+        public int Row
+        {
+            get => m_Row;
+            set => m_Row = value;
+        }
+        public int Column
+        {
+            get => m_Column;
+            set => m_Column = value;
+        }
+    }
+}
